e561d2ef9a8c6b4ec6d4945fc90d5037bc35f00b
subin <subinshrestha429@gmail.com>
2021-07-22 15:19:57 +0545

    Dark Theme transition added

---
+++ assests/js/index.js
@@ -0,0 +1,33 @@
+var themeButton = document.getElementById('theme-switch');
+var dataThemeElements = document.querySelectorAll('[data-theme]');
+
+var theme = localStorage.getItem('current-theme');
+if (!theme) {
+	localStorage.setItem('current-theme', 'light');
+	theme = localStorage.getItem('current-theme');
+}
+
+var trans = () => {
+	document.documentElement.classList.add('transition');
+	window.setTimeout(() => {
+		document.documentElement.classList.remove('transition');
+	}, 1000);
+};
+
+themeButton.addEventListener('click', function () {
+	var currentTheme = localStorage.getItem('current-theme');
+
+	if (currentTheme === 'light') {
+		localStorage.setItem('current-theme', 'dark');
+		dataThemeElements.forEach((item) => {
+			trans();
+			item.setAttribute('data-theme', 'dark');
+		});
+	} else {
+		localStorage.setItem('current-theme', 'light');
+		dataThemeElements.forEach((item) => {
+			trans();
+			item.setAttribute('data-theme', 'light');
+		});
+	}
+});
