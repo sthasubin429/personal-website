b1d7883ab4a88a196e8772d37bc90d7d52a718fb
subin <subinshrestha429@gmail.com>
2022-08-08 01:01:08 +0545

    feat: add build script

---
+++ build_deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+pwd
+ls
+
+# delete dist folder
+sudo rm -rf dist/*
+
+# run build command
+npm run build
+
+# copy CNAME into dist
+cp CNAME dist/
+
+echo "Deploying ..."
+
+# run gh-pages deploy command
+npm run deploy
